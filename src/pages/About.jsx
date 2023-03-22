c24c864f95bcc1c0409f5d150894ee7e28911bf5
Liv <liv.zana@gmail.com>
2023-03-22 16:06:41 -0400

    Home page, about page, not found page draft

---
+++ src/pages/About.jsx
@@ -0,0 +1,16 @@
+function About() {
+  return (
+    <div>
+      <h1 className="text-6xl mb-4">
+        Github Finder
+        
+      </h1>
+      <h2 className="text-4xl mb-5">About This App</h2>
+      <p>An educational React app to search GitHub profiles.</p>
+      <p>Version: 1.0.0</p>
+        
+    </div>
+  )
+}
+
+export default About
