d570cd64d07054fe3f599ea88ebbaeb3fd907eb9
Liv <liv.zana@gmail.com>
2023-03-22 15:44:08 -0400

    Initial commit containing header component

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,15 @@
+function Footer() {
+  const footerYear = new Date().getFullYear()
+
+
+  return (
+    <footer className="footer p-10 bg-gray-700 text-primary-content footer-center">
+      <div>
+        
+      </div>
+
+    </footer>
+  )
+}
+
+export default Footer
